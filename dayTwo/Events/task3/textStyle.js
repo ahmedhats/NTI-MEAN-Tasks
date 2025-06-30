136b25154411b50fb7c68cdb495f1357aebce8fe
ahmedhat <2023170001@cis.asu.edu.eg>
2025-06-30 23:51:23 +0300

    all event tasks completed

---
+++ dayTwo/Events/task3/textStyle.js
@@ -0,0 +1,36 @@
+const PAR = document.getElementById('PAR');
+function ChangeFont(font) {
+    PAR.style.fontFamily = font;
+}
+
+function ChangeAlign(align) {
+    PAR.style.textAlign = align;
+}
+
+function ChangeHeight(height) {
+    PAR.style.lineHeight = height;
+}
+
+function ChangeLSpace(space) {
+    PAR.style.letterSpacing = space;
+}
+
+function ChangeIndent(indent) {
+    PAR.style.textIndent = indent;
+}
+
+function ChangeTransform(transform) {
+    PAR.style.textTransform = transform;
+}
+
+function ChangeDecorate(decorate) {
+    PAR.style.textDecoration = decorate;
+}
+
+function ChangeBorder(border) {
+    PAR.style.borderStyle = border;
+}
+
+function ChangeBorderColor(color) {
+    PAR.style.borderColor = color;
+} 
